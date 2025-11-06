fee2d327399787e49bc5ed2edaa6d5f846cba93a
estudiante <estudiante@DAM1-87d315>
2025-11-06 11:48:14 +0100

    Versión 2

---
+++ ExamenUD2/src/es/oretania/SumaEnteros.java
@@ -0,0 +1,19 @@
+package es.oretania;
+
+public class SumaEnteros {
+	
+	public static void main(String[] args) {
+		try {
+			int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			int resultado = calcularSuma(numeros);
+	} catch (Exception e) {
+	}
+}
+	public static int calcularSuma(int[] valores) throws Exception {
+		int suma = 0;
+		for (int i = 0; i < valores.length; i++) {
+			suma += valores[i];
+		}
+		return suma;
+	}
+}
